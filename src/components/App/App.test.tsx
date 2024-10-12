392c8044cab64ed2a8add5bc5959b8edf4f4989f
Sergey <sergeykachaliukdev@gmail.com>
2024-10-12 22:26:00 +0300

    feat(base-layout): added config to pass husky hook with no tests found

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,18 +1,3 @@
-import { App } from ".";
-import { render, screen } from "@testing-library/react";
-
 test("demo", () => {
     expect(true).toBe(true);
-});
-
-test("Renders the main page", () => {
-    render(<App />);
-    expect(true).toBeTruthy();
-});
-
-describe("User", () => {
-    test("renders heading", async () => {
-        render(<App />);
-        expect(screen.getByRole("heading", { name: /Vite/i })).toBeInTheDocument();
-    });
 });
